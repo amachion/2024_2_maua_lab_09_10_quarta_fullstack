d0d23eef68fced70d74ccd9e0ec6c3291ea2c932
Andreia <andreia.machion@saojudas.br>
2024-11-06 12:46:12 -0300

    modal login até p. 96

--- front/js/frontend.js
+++ front/js/frontend.js
@@ -16,6 +16,24 @@ function listarFilmes (filmes) {
         celulaSinopse.innerHTML = filme.sinopse
     }
 }
+function exibirAlerta (seletor, innerHTML, classesToAdd, classesToRemove, timeout) {
+    let alert = document.querySelector(seletor)
+    alert.innerHTML = innerHTML
+    alert.classList.add(...classesToAdd)
+    alert.classList.remove(...classesToRemove)
+    setTimeout(() => {
+        alert.classList.remove(...classesToAdd)
+        alert.classList.add(...classesToRemove)
+    }, timeout)
+}
+function esconderModal (seletor, timeout) {
+    setTimeout(() => {
+        let modal = bootstrap.Modal.getInstance(
+            document.querySelector(seletor)
+        )
+        modal.hide()
+    }, timeout)
+}
 async function obterFilmes() {
     const filmesEndpoint = '/filmes'
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
@@ -41,12 +59,67 @@ async function cadastrarFilme() {
         listarFilmes(filmes)
     }
     else {
-        let alert = document.querySelector('.alert')
-        alert.classList.add('show')
-        alert.classList.remove('d-none')
-        setTimeout(() => {
-            alert.classList.remove('show')
-            alert.classList.add('d-none')
-        }, 2000)
+        exibirAlerta('.alert-filme', "Preencha todos os campos!", 
+            ['show', 'alert-danger'], ['d-none'], 2000
+        )
+    }
+}
+async function cadastrarUsuario() {
+    let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
+    let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
+    let usuarioCadastro = usuarioCadastroInput.value
+    let passwordCadastro = passwordCadastroInput.value
+    if (usuarioCadastro && passwordCadastro) {
+        try {
+            const cadastroEndpoint = '/signup'
+            const URLcompleta = `${protocolo}${baseURL}${cadastroEndpoint}`
+            await axios.post(
+                URLcompleta,
+                {login: usuarioCadastro, password: passwordCadastro}
+            )
+            usuarioCadastroInput.value = ""
+            passwordCadastroInput.value = ""
+            exibirAlerta('.alert-modal-cadastro', "Usuário cadastrado com sucesso!!!", ['show', 'alert-success'], ['d-none'], 2000)
+            esconderModal('#modalCadastro', 2000)
+        }
+        catch (e) {
+            usuarioCadastroInput.value = ""
+            passwordCadastroInput.value = ""
+            exibirAlerta('.alert-modal-cadastro', "Não foi possível cadastrar!!!", ['show', 'alert-danger'], ['d-none'], 2000)
+            esconderModal('#modalCadastro', 2000)
+        }
+    }    
+    else {
+        exibirAlerta('.alert-modal-cadastro', "Preencha todos os campos!!!", ['show', 'alert-danger'], ['d-none'], 2000)
+    }
+}
+const loginUsuario = async () => {
+    let usuarioLoginInput = document.querySelector('#usuarioLoginInput')
+    let passwordLoginInput = document.querySelector('#passwordLoginInput')
+    let usuarioLogin = usuarioLoginInput.value
+    let passwordLogin = passwordLoginInput.value
+    if (usuarioLogin && passwordLogin) {
+        try {
+            const loginEndpoint = '/login'
+            const URLcompleta = `${protocolo}${baseURL}${loginEndpoint}`
+            const response = await axios.post (
+                URLcompleta,
+                {login: usuarioLogin, password: passwordLogin}
+            )
+            usuarioLoginInput.value = ""
+            passwordLoginInput.value = ""
+            exibirAlerta('.alert-modal-login', "Login realizado com sucesso!", ['show', 'alert-success'], ['d-none'], 2000)
+            esconderModal('#modalLogin', 2000)
+            const cadastrarFilmeButton = document.querySelector('#cadastrarFilmeButton')
+            cadastrarFilmeButton.disabled = false
+        }
+        catch (e) {
+
+        }
+    }
+    else {
+        exibirAlerta('.alert-modal-login', 'Preencha todos os campos!',
+                    ['show', 'alert-danger'], ['d-none'], 2000
+        )
     }
 }
